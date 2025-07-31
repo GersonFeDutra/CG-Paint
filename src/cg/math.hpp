4fe2585833cf01c44d1ea74c2784e87b1523ee12
Dutra, Gerson <gersonfedutra@gmail.com>
2025-07-31 16:39:01 -0300

    feat(math): matrix3
    Add `Mat3` template class for 2D transformations.

--- src/cg/math.hpp
+++ src/cg/math.hpp
@@ -27,7 +27,13 @@ constexpr T PI = std::numbers::pi_v<T>;
 
 template <typename T>
 struct Vec2 {
-    T x = 0, y = 0;
+    union {
+        struct {
+            T x = 0, y = 0;
+        };
+        T coords[2];
+        T rows[2];
+    };
 
     // Construtores
     constexpr Vec2() = default;
@@ -35,7 +41,7 @@ struct Vec2 {
 
     // Construtor de conversão (somente quando U→T é válido):
     template<std::convertible_to<T> U>
-    Vec2(Vec2<U> const& v) : x(v.x) {}
+    constexpr Vec2(Vec2<U> const& v) : x(v.x) {}
     // De forma similar para o operador de assinatura:
     template<std::convertible_to<T> U>
     Vec2& operator=(Vec2<U> const& v) {
@@ -75,11 +81,18 @@ using Vector2i = Vec2<int>;
 
 template <typename T>
 struct Vec3 {
-    T x = 0.0f, y = 0.0f, z = 0.0f;
+    union {
+        struct {
+            T x = 0.0f, y = 0.0f, z = 0.0f;
+        };
+        T coords[3];
+        T rows[3];
+    };
 
     // Construtores
     constexpr Vec3() = default;
-    Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+    constexpr Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+    constexpr Vec3(Vec2<T> v, T z = 0) : x(v.x), y(v.y), z(z) {}
 
     // Construtor de conversão (somente quando U→T é válido):
     template<std::convertible_to<T> U>
@@ -125,6 +138,81 @@ using Vector3 = Vec3<float>;
 using Vector3i = Vec3<int>;
 
 
+template <typename T>
+struct Mat3 /* Matriz column-major 3x3. Representing a 2D transformation. */
+{
+    Vec3<T>columns[3];
+
+    constexpr Mat3() = default;
+    constexpr Mat3(Vec3<T> col_v1, Vec3<T> col_v2, Vec3<T> col_v3) : columns( col_v1, col_v2, col_v3 ) {}
+    constexpr Mat3(Vec2<T> translation) : columns( {1, 0, 0}, {0, 1, 0}, {translation, 1}) {}
+    constexpr Mat3(Vec2<T> v1_xaxis, Vec2<T> v2_yaxis) : columns( Vec3<T>{v1_xaxis.x}, Vec3<T>{v2_yaxis.x}, Vec3<T>{0, 0, 1}) {}
+    constexpr Mat3(T x1_x, T x2_y, T x3_h, T y1_x, T y2_y, T y3_h, T h1_x, T h2_y, T h3_h) : columns{ x1_x, x2_y, x3_h, y1_x, y2_y, y3_h, h1_x, h2_y, h3_h } {}
+
+    static constexpr inline Mat3 translation(Vec2 direction, Mat3 m) {
+        return {
+            Vec3{ 1, 0, 0 },
+            Vec3{ 0, 1, 0 },
+            Vec3{ direction.x, direction.y, 1 },
+        } * m;
+    }
+    constexpr inline Mat3& translate(Vec2 direction) {
+        return *this *= {
+            Vec3{ 1, 0, 0 },
+            Vec3{ 0, 1, 0 },
+            Vec3{ direction.x, direction.y, 1 },
+        };
+    }
+    static constexpr inline Mat3& rotation(float angle, Mat3 m) {
+        return {
+            Vec3{ cosf(angle), sinf(theta), 0 },
+            Vec3{ -sinf(angle), cosf(theta), 0 },
+            Vec3{ 0, 0, 1 },
+        } * m;
+    }
+    constexpr inline Mat3& rotate(float angle) {
+        return *this *= {
+            Vec3{ cosf(angle), sinf(theta), 0 },
+            Vec3{ -sinf(angle), cosf(theta), 0 },
+            Vec3{ 0, 0, 1 },
+        };
+    }
+
+    constexpr inline const T get(std::size_t col, std::size_t row) const {
+        return columns[col].rows[row];
+    }
+
+    constexpr inline T& get(std::size_t col, std::size_t row) {
+        return columns[col].rows[row];
+    }
+
+    constexpr inline T& set(std::size_t col, std::size_t row, T value) {
+        return columns[col].rows[row] = value;
+    }
+
+};
+
+
+// Point transformation.
+template<typename T, std::convertible_to<T> U>
+inline Vec3<std::common_type<T, U>> operator*(const Mat3<T>& m, const Vec3<U>& v) {
+    return {
+        m.columns[0].x * v.x + m.columns[1].x * v.y + m.columns[2].x * v.z,
+        m.columns[0].y * v.x + m.columns[1].y * v.y + m.columns[2].y * v.z,
+        m.columns[0].z * v.x + m.columns[1].z * v.y + m.columns[2].z * v.z
+    };
+}
+
+
+template <typename T, std::convertible_to<T> U>
+inline Mat3<T> operator*(Mat3<T> a, Mat3<U> b) {
+    m.columns[0] = {a * b.columns[0]};
+    m.columns[1] = {a * b.columns[1]};
+    m.columns[2] = {a * b.columns[2]};
+    return a;
+}
+
+
 struct ColorRgb {
     unsigned char r = 0, g = 0, b = 0;
 
@@ -149,5 +237,8 @@ inline void randomizeColor()
         (std::rand() % 256)
     );
 }
+
+using Transform2D = Mat3<float>;
+using Matrix3i = Mat3<int>;
 
 } // namespace cg
